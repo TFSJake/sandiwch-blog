8249c84d6f20787fcf7d25387ecc464646b1d70c
jake <tfseo@gmail.com>
2022-05-08 11:25:36 -0500

    Working Kafaka broker.

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
+
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -20,5 +21,5 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have beecuntn seen {} times.\n'.format(count)
+    return f'Hello World! I have been seen {count} times.\n'
 
